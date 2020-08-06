e7f113a36ab719097215c87525e745d75b7b5ee5
karma105 <karim.mammadov@gmail.com>
2020-08-05 20:27:49 -0400

    Reverse

--- src/utility/ZombieAttack.java
+++ src/utility/ZombieAttack.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class ZombieAttack {
 
 
+
         public static void main(String[] args) {
             Scanner input = new Scanner(System.in);
             int[] inhabitants = new int[8];
