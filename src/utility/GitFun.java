561435f17df4f3513e0d4ac70904692d897eddcf
karma105 <68972556+karma105@users.noreply.github.com>
2020-08-05 21:34:12 -0400

    Update GitFun.java

--- src/utility/GitFun.java
+++ src/utility/GitFun.java
@@ -2,6 +2,6 @@ package utility;
 
 public class GitFun {
     public static void main(String[] args) {
-        System.out.println("Everything is fun");
+        System.out.println("Everything is fun and nice");
     }
 }
